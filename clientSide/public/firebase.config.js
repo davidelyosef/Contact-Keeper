1aa67bb551983f221e05d456e111dc799ad74129
David el Yosef <davidyf96@gmail.com>
2020-09-21 18:47:30 +0300

    ready to deploy

---
+++ clientSide/public/firebase.config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBP5teyzkFhwgy2aVYex0ci3YYjuFvE8qo",
+  authDomain: "contact-keeper-7161b.firebaseapp.com",
+  databaseURL: "https://contact-keeper-7161b.firebaseio.com",
+  projectId: "contact-keeper-7161b",
+  storageBucket: "contact-keeper-7161b.appspot.com",
+  messagingSenderId: "398892735848",
+  appId: "1:398892735848:web:07fffc7a8fd2a8ace94add",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Serve index.html:
+app.use(express.static(path.join(__dirname, "./clientSide/build")));
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
@@ -15,13 +18,18 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Any other route - return index.html as we are SPA:
+app.use("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "./clientSide/build/index.html"));
+});
+
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('clientSide/build'));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'clientSide', 'build', 'index.html'))
   );
 }
 
